c315539e0dcde398496c30b4ecb9278d10d0d708
sukhd <devappplay@gmail.com>
2023-10-24 12:49:57 +0530

    check test2

--- app/src/main/java/com/example/taskflow/activities/ExtensionFunctions.kt
+++ app/src/main/java/com/example/taskflow/activities/ExtensionFunctions.kt
@@ -1,5 +1,11 @@
 package com.example.taskflow.activities
 
+import android.view.View
+
 fun clickMthof(){
   var xyz = "m"
+}
+
+fun View.myclicks(block : ()-> Unit)=setOnClickListener{
+  block.invoke()
 }
